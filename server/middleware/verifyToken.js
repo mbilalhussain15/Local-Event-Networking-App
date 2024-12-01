6c9576afa1f396e4f97e927d81f17cafd40e746c
Zabi Ansari <123079863+zabiansari7@users.noreply.github.com>
2024-12-01 23:25:24 +0100

    updated VerifyToken

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -1,19 +1,20 @@
-import { TokenManager } from '../utils/jwtTokenManager.js';  // Adjust the path based on your project structure
+import { TokenManager } from '../utils/jwtTokenManager.js';
 
-// Middleware to check if the token is valid
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.token;  // Get the token from the cookies
+  const token = req.cookies.token;  // Get token from cookie
 
   if (!token) {
-    return res.status(401).json({ message: 'Authorization token is missing.' });
+    return res.status(401).json({ message: 'Token is missing.' });
   }
 
   try {
-    // Verify the token
+    // Decode and verify the token
     const decoded = TokenManager.verifyToken(token);
-    req.user = decoded;  // Attach decoded user data to the request object
-    next();  // Proceed to the next middleware or route handler
+
+    // Attach the decoded token data to the request object for further use
+    req.user = decoded;
+    next();
   } catch (error) {
-    res.status(401).json({ message: 'Invalid or expired token.' });
+    res.status(401).json({ message: error.message });
   }
 };
