a065ab226baea86eff31402e98463fd7b0fddd29
Zabi Ansari <123079863+zabiansari7@users.noreply.github.com>
2024-12-01 23:25:37 +0100

    added api for changing password

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,8 +7,8 @@ import {
   logout, 
   protectedRoute, 
   register,
+  updatePassword,
   updateUserLanguage, 
-  changePassword,
   updateUser 
 } from '../controllers/userController.js';
 import { verifyToken } from '../middleware/verifyToken.js'
@@ -24,7 +24,8 @@ router.get('/users',verifyToken, getUsers);
 router.get('/users/:id',verifyToken, getUser);     
 router.put('/users/:id',verifyToken, updateUser);      
 router.delete('/users/:id',verifyToken, deleteUser);
+
 router.put('/users/:id/language', verifyToken, updateUserLanguage);
-router.post('/change-password', verifyToken, changePassword);
+router.put('/updatePassword/:userId', updatePassword);
 
 export default router;
