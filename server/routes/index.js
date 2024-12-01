7e4505b9105ac17ea6c37f813babb207711cb14e
Muhammad <muhammad.hussain@cardi-link.com>
2024-12-02 00:39:27 +0100

    updated index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import userRoutes from "./userRoutes.js";
 import eventRoutes from './eventRoutes.js'
 import ticketRoutes from './ticketRoutes.js';
+import paymentRoutes from './paymentRoutes.js';
 
 const router = express.Router();
 
 router.use("/event",eventRoutes); //api/event
 router.use("/tickets",ticketRoutes); //api/ticket
+router.use("/payments",paymentRoutes); //api/payment
 
 
 
