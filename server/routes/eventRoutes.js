f8d960d652ec722282027d75c92c731e1c23ebdf
Muhammad <muhammad.hussain@cardi-link.com>
2024-12-02 03:47:47 +0100

    added location API

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,27 +1,31 @@
-import express from "express";
-
-import { 
+import express from 'express';
+import {
     createEvent,
     getAllEvents,
     getEventById,
     updateEvent,
     deleteEvent,
- } from "../controllers/eventController.js";
+    updateEventLocation,
+} from '../controllers/eventController.js';
 
 const router = express.Router();
 
-router.post("/createEvent", createEvent);
+// Create event
+router.post('/createEvent', createEvent);
 
 // Get all events
-router.get("/getEvents", getAllEvents);
+router.get('/getEvents', getAllEvents);
 
-// Get an event by ID
-router.get("/getEventById/:id", getEventById);
+// Get event by ID
+router.get('/getEventById/:id', getEventById);
 
-// Update an event by ID
-router.put("/updateEvent/:id", updateEvent);
+// Update event
+router.put('/updateEvent/:id', updateEvent);
 
-// Delete an event by ID
-router.delete("/deleteEvent/:id", deleteEvent);
+// Delete event
+router.delete('/deleteEvent/:id', deleteEvent);
+
+// Update event location only
+router.put('/updateEventLocation/:id', updateEventLocation);
 
 export default router;
