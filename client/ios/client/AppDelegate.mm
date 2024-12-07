14b9008ddcd9529392dea2f12f53ec4b40e66486
Zabi Ansari <123079863+zabiansari7@users.noreply.github.com>
2024-12-07 17:32:44 +0100

    updated appDelegate.mm file

--- client/ios/client/AppDelegate.mm
+++ client/ios/client/AppDelegate.mm
@@ -2,10 +2,13 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyBUuX_g_HY-fYv0D5P9gLP1e4imPTxybCo"];
   self.moduleName = @"client";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
