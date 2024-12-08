d1d3351ea54fe0c95cba1d8c1d4f8c952c0fccc7
Muhammad <muhammad.hussain@cardi-link.com>
2024-12-08 22:06:22 +0100

    added profile screen routes

--- client/src/navigation/RootNavigator.jsx
+++ client/src/navigation/RootNavigator.jsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen';
 import RegistrationScreen from '../screens/RegistrationScreen';
 import BottomTabs from './BottomTabs'; // Your tab navigator
+import ProfileScreen from '../screens/ProfileScreen';
 
 const Stack = createStackNavigator();
 
@@ -16,6 +17,7 @@ const RootNavigator = ({ isAuthenticated }) => {
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="RegisterScreen" component={RegistrationScreen} />
       <Stack.Screen name="Main" component={BottomTabs} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
     </Stack.Navigator>
   );
 };
