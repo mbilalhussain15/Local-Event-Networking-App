9a648a4be1f7f146381b389284d11d804ee7e28a
Zabi Ansari <123079863+zabiansari7@users.noreply.github.com>
2024-12-10 06:00:54 +0100

    updated eventModel.js

--- server/models/eventModel.js
+++ server/models/eventModel.js
@@ -7,8 +7,6 @@ const locationSchema = new mongoose.Schema({
     state: { type: String },
     postalCode: { type: String, required: true },
     country: { type: String, required: true },
-    latitude: { type: Number, required: true },
-    longitude: { type: Number, required: true },
 });
 
 const eventSchema = new mongoose.Schema({
@@ -18,12 +16,12 @@ const eventSchema = new mongoose.Schema({
     maxCapacity: { type: Number, required: true },
     totalTicketsSold: { type: Number, default: 0 },
     registration_required: { type: Boolean },
-    contact_email: { type: String, required: true },
     is_virtual: { type: Boolean },
-    created_by: { type: String, required: true },
+    user_id: { type: String, required: true }, // Changed from created_by to user_id
     eventImage: { type: String }, // URL of the locally saved image
     date: { type: Date, required: true },
     location: { type: locationSchema, required: true }, // Location subdocument
+    createdAt: { type: Date, default: Date.now }, // Added createdAt attribute
 });
 
 export default mongoose.model('Event', eventSchema);
