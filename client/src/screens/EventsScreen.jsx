15ee12476f7f283718770a4dc5c36ac7b0ebf772
Zabi Ansari <123079863+zabiansari7@users.noreply.github.com>
2024-12-11 17:06:29 +0100

    updated EventsScreen

--- client/src/screens/EventsScreen.jsx
+++ client/src/screens/EventsScreen.jsx
@@ -2,8 +2,10 @@ import { useGetEventsQuery } from '../redux/slices/api/eventApiSlice';
 import React, { useEffect, useState } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';// Ensure correct path
 import EventCard from '../components/EventCard';
+import { useTranslation } from 'react-i18next'; 
 
 const EventsScreen = ({ navigation }) => {
+  const { t } = useTranslation();
   const [events, setEvents] = useState([]); // Local state for events
   const { data, isLoading, isError } = useGetEventsQuery(); // Fetch events from API
 
@@ -15,18 +17,22 @@ const EventsScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Events</Text>
+      <Text style={styles.title}>{t('events')}</Text> 
       <FlatList
         data={events}
         keyExtractor={(item) => item._id}
-        
         renderItem={({ item }) => (
           <EventCard
             key={item._id}
             eventName={item.eventName}
             category={item.category}
             location={item.location}
-            onPress={() => navigation.navigate('eventDetails', { eventId: item._id, eventDetailByID: item })}
+            onPress={() =>
+              navigation.navigate('eventDetails', {
+                eventId: item._id,
+                eventDetailByID: item,
+              })
+            }
           />
         )}
       />
